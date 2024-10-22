4839ace8df525f3a8afa20f37c1a385192ab5e8f
DesertChaos <vadimsysolin52565@gmail.com>
2024-10-22 12:13:43 +0300

    MAIN COMMIT

--- Program.cs
+++ Program.cs
@@ -1,21 +1,75 @@
-﻿Console.WriteLine("Введите число x в 10-тичной системе счисления: ");
+﻿
+Console.WriteLine("Введите число x в q1-тичной системе счисления: ");
 int x = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите систему счисления q в отрезке [2;9]: ");
-int q = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите систему счисления q1 в отрезке [2;10]: ");
+int q1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите систему счисления q2 в отрезке [2;10]: ");
+int q2 = int.Parse(Console.ReadLine());
 
-if (q < 2 || q > 9)
+if (q1 < 2 || q1 > 10 || q2 < 2 || q2 > 10)
 {
-    Console.WriteLine("Ошибка: Основание системы счисления должно быть в диапазоне от 2 до 9.");
+    Console.WriteLine("Ошибка: Основание системы счисления должно быть в диапазоне от 2 до 10.");
     return;
 }
 
+if (q1 == 10)
+{
+    string str = "";
 
-string s;
+    int a = 0;
+    int b = x;
+    while (b > 0)
+    {
+        a = b % q2;
+        b = b / q2;
+        str += a.ToString();
+    }
+    string reversed = new string(str.Reverse().ToArray());
+    Console.Write(reversed);
+}if (q2 == 10)
+{
+    string len = Convert.ToString(x);
+    int lenint = len.Length - 1;
+    double aa = 0;
+    int bb = x;
+    while (bb > 0)
+    {
+        x = x % 10;
+        bb = bb / 10;
+        aa += x * Math.Pow(q1, lenint);
+        lenint -= 1;
+    }
+    Console.Write(aa);
+}else
+{
+    string len = Convert.ToString(x);
+    int lenint = len.Length - 1;
+    double aa = 0;
+    int bb = x;
+    while (bb > 0)
+    {
+        x = x % 10;
+        bb = bb / 10;
+        aa += x * Math.Pow(q1, lenint);
+        lenint -= 1;
+    }
+    string str = "";
 
-int a = 0;
-int b = x;
-while(b>0){
-    a = b % 10;
-    b = b / 10;
-    Console.WriteLine(a);
+    double a = 0;
+    double b = aa;
+    while (b > 0)
+    {
+        a = b % q2;
+        b = b / q2;
+        str += a.ToString();
+    }
+    string reversed = new string(str.Reverse().ToArray());
+    Console.Write(reversed);
 }
+   
+
+
+
+
+
+
--- obj/Debug/net8.0/ConsoleApp8.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5d9140d74fc2c5b0d5dd481dbf8a5489a091235")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
